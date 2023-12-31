256d22e681ac2e49b2089a34602e93b7efddaf08
Felipe Lagos <felagossanchez@gmail.com>
2023-12-31 12:57:15 -0300

    Update Vite configuration and package dependencies

---
+++ src/components/Generator/Generator.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import { describe, test } from "vitest";
+
+import { Generator } from "./Generator";
+
+
+describe("Generator Test", () => {
+  test("renders headline", () => {
+    render(<Generator />);
+  });
+
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react({ fastRefresh: false })],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./test/setup.ts"],
+    testMatch: ['./src/**/*.spec.tsx'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    }
+  },
+});
